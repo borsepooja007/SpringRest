0fbe7cf8121cabe217324f2dd729365af869dfa4
borsepooja007 <59304709+borsepooja007@users.noreply.github.com>
2021-05-15 12:40:03 +0530

    Add files via upload

---
+++ employee/src/main/java/org/example/fifth/employee/Service/EmployeeService.java
@@ -0,0 +1,12 @@
+package org.example.fifth.employee.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface EmployeeService {
+
+	
+	
+	
+	
+}
---
+++ employee/src/test/java/org/example/fifth/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fifth.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
